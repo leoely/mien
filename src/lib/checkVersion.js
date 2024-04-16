e21e70ea001fb6a5f951fb063b31d237f2ac3788
Leo Brad <>
2024-04-16 21:53:15 +0800

    [Change] Change checkVersion encapsulation.

--- src/lib/checkVersion.js
+++ src/lib/checkVersion.js
@@ -7,23 +7,26 @@ function greaterOrEqualVersion(v1, v2) {
   return !result.some((flag) => !flag);
 }
 
-const v = execSync('node -v').toString();
-if (!greaterOrEqualVersion(v.substring(0, v.length - 1), 'v21.6.2')) {
-  console.error([
-    '',
-    chalk.bold('The version of node currently used is lower:'),
-    '',
-    'please use node version greater or equal than v19.3.0.',
-    'use command `' + chalk.bold('node -v') + '` check current version of node.',
-    '',
-    chalk.bold('the following are possible solutions:'),
-    '',
-    ' - visit the officical website of node to download and install the latest version node.',
-    '   officical website address of node is ' + chalk.bold('https://nodejs.org'),
-    '',
-    ' - use command line tool `' + chalk.bold('nvm') + '` install and use lastest version node.',
-    '   github address of nvm is ' + chalk.bold('https://github.com/nvm-sh/nvm') + '.',
-    '',
-  ].join('\n'));
-  process.exit(0);
+export default function checkVersion() {
+  const v = execSync('node -v').toString();
+  if (!greaterOrEqualVersion(v.substring(0, v.length - 1), 'v21.6.2')) {
+    console.error([
+      '',
+      chalk.bold('The version of node currently used is lower:'),
+      '',
+      'please use node version greater or equal than v19.3.0.',
+      'use command `' + chalk.bold('node -v') + '` check current version of node.',
+      '',
+      chalk.bold('the following are possible solutions:'),
+      '',
+      ' - visit the officical website of node to download and install the latest version node.',
+      '   officical website address of node is ' + chalk.bold('https://nodejs.org'),
+      '',
+      ' - use command line tool `' + chalk.bold('nvm') + '` install and use lastest version node.',
+      '   github address of nvm is ' + chalk.bold('https://github.com/nvm-sh/nvm') + '.',
+      '',
+    ].join('\n'));
+    process.exit(0);
+  }
 }
+
